220acf225c088200d8c599dc9573c37177d3451b
Baher-Essam <baher.essam@gmail.com>
2021-01-12 14:34:13 +0200

    Adding portfolio section

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  //defult active list *all*
+  $('.list').first().addClass('active');
+  
+  $('.list').click(function(){
+    const projectList = $(this).attr('data-filter');
+    if(projectList == 'all')
+    {
+      $('.protfolio-item').show('300');
+    }
+    else
+    {
+      $('.protfolio-item').not('.'+projectList).hide('300' ,function(){
+        $('.protfolio-item').filter('.'+projectList).show('300');
+      });
+    
+    }
+  })
+  // add active class on selected button
+  $('.list').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+})
